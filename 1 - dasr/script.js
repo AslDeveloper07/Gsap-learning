b968c1bac4f33b6d22ff4e8e6ed9548950a4b7ce
AslDeveloper07 <aslwexking777@gmail.com>
2025-06-21 15:38:59 +0500

    das

--- 1 - dasr/script.js
+++ 1 - dasr/script.js
@@ -1,73 +1,61 @@
-
-
-
 // Oddiy animatsiya berish
 
-// gsap.to("#card1",{
-//     y:300,
-//     delay:1,
-//     duration:2,
-//     rotate: 180,
-//     backgroundColor: "orange",
-//   yoyo: true,
-// stagger:2,
+gsap.to("#card1", {
+  y: 300,
+  delay: 1,
+  duration: 2,
+  rotate: 180,
+  backgroundColor: "orange",
+  yoyo: true,
+  stagger: 2,
+});
 
-// })
-
-// gsap.to("#card2",{
-//     y:600,
-//     delay:2,
-//     duration:1,
-//     rotate: 360,
-//     backgroundColor: "green",
-//   yoyo: true,
-// stagger:2,
-
-// })
-// gsap.to("#card3",{
-//     y:300,
-//     delay:3,
-//     duration:1,
-//     rotate: 180,
-//     backgroundColor: "red",
-//   yoyo: true,
-// stagger:2,
-// })
-
-
+gsap.to("#card2", {
+  y: 600,
+  delay: 2,
+  duration: 1,
+  rotate: 360,
+  backgroundColor: "green",
+  yoyo: true,
+  stagger: 2,
+});
+gsap.to("#card3", {
+  y: 300,
+  delay: 3,
+  duration: 1,
+  rotate: 180,
+  backgroundColor: "red",
+  yoyo: true,
+  stagger: 2,
+});
 
 // Oddiy animatsiyadan keyinchi holat
 
+var tl = gsap.timeline();
+tl.to("#card1", {
+  x: 600,
+  delay: 3,
+  duration: 1,
+  rotate: 180,
+});
 
+var tl = gsap.timeline();
+tl.to("#card2", {
+  x: 600,
+  delay: 3,
+  duration: 1,
+  rotate: 180,
+});
 
-// var tl= gsap.timeline()
-// tl.to("#card1",{
-//     x:600,
-//     delay:3,
-//     duration:1,
-//     rotate: 180,
-// })
-
-// var tl= gsap.timeline()
-// tl.to("#card2",{
-//     x:600,
-//     delay:3,
-//     duration:1,
-//     rotate: 180,
-// })
-
-// var tl= gsap.timeline()
-// tl.to("#card3",{
-//     x:100,
-//     delay:3,
-//     duration:1,
-//     rotate: 180,
-// })
-
-
+var tl = gsap.timeline();
+tl.to("#card3", {
+  x: 100,
+  delay: 3,
+  duration: 1,
+  rotate: 180,
+});
 
 // h1 textiga animatsiya berish
-
 
 // gsap.from("h1", {
 //   y: 20,
