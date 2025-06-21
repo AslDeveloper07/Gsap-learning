ac32b4d9751990b6fe17ccac7c3d422593f64a2b
AslDeveloper07 <aslwexking777@gmail.com>
2025-06-21 21:55:28 +0500

    asa

---
+++ 3 - dars/script.js
@@ -0,0 +1,20 @@
+var svgOriginButton = document.getElementById("svgOrigin"),
+    transformOriginButton = document.getElementById("transformOrigin"),
+    tweenCode = document.getElementById("tweenCode"),
+    boxes = document.querySelectorAll(".box"),
+    tween = gsap.to({}, {});
+
+function svgOriginRotation() {
+  tween.seek(0).kill(); //reset
+  tween = gsap.to(boxes, {duration: 1, rotation: 360, svgOrigin: "300 200"});
+  tweenCode.innerHTML = 'svgOrigin: "300 200"';
+}
+
+function transformOriginRotation() {
+  tween.seek(0).kill(); //reset
+  tween = gsap.to(boxes, {duration: 1, rotation: 360, transformOrigin: "50% 50%"});
+  tweenCode.innerHTML = 'transformOrigin: "50% 50%"'
+}
+
+svgOriginButton.onclick = svgOriginRotation;
+transformOriginButton.onclick = transformOriginRotation;
